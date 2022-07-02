22616847a2bfdc663b6def9c9cbb30db2bf8cffc
coderpriya-raj <108212793+coderpriya-raj@users.noreply.github.com>
2022-07-02 16:32:49 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,20 @@
+#a=int(input ("enter the number "))
+#if (a%2==0):
+ #else:
+  #print (str(a)+ " = odd number")
+
+from typing import AsyncGenerator
+
+
+a= int (input("enter your age ")) 
+b= str (input("enter your name "))
+print (b)
+if (a>18):
+  print ("you can vote ") 
+  
+elif (a==18):
+  print ("you are on border line" )
+  
+elif (a< 18):
+  print ("Sorry ,you are not eligile")
+     
